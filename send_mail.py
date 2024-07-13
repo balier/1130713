4638f73caaa3925efb47d681c49c161d689f883d
Tony <tony@sssde.com>
2024-07-13 16:09:10 +0800

    send mail v2

--- send_mail.py
+++ send_mail.py
@@ -4,21 +4,24 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 sender = "wzh609@gmail.com"
-receiver = "psp4588@gmail.com"
-msg = MIMEMultipart()
-msg["From"] = sender
-msg["To"] = receiver
-heard = Header("Test Send Email","utf-8")
-msg["Subject"] = heard.encode() #編碼
+receiver = ["psp4588@gmail.com","wzh609@gmail.com"]
 
 
-boby = "This is email send from python"
-msg.attach(MIMEText(boby))
-# mboy=MIMEText(boby)
-# msg.attach(mboy)
+for i in receiver:
 
-ssl_context = ssl.create_default_context() #連線設定
-with smtplib.SMTP_SSL("smtp.gmail.com",465,context =ssl_context) as server: #把ssl_context default值改掉，指定context=ssl_context
-    server.login(sender,"knkoypfcmfkghjju")
-    server.sendmail(sender,receiver,msg.as_string())
-print ("Success send email")
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = i
+    heard = Header("Test Send Email","utf-8")
+    msg["Subject"] = heard.encode() 
+
+
+    boby = "This is email send from python"
+    msg.attach(MIMEText(boby))
+
+
+    ssl_context = ssl.create_default_context() 
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context =ssl_context) as server: 
+        server.login(sender,"knkoypfcmfkghjju")
+        server.sendmail(sender,i,msg.as_string())
+    print ("Success send email")
